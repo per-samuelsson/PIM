80c202c1ef17c428e28f57296423ee5265698431
Per Samuelsson <per@starcounter.com>
2015-12-03 10:33:02 +0100

    Added PIM to the launcher menu.

--- src/PIM/Program.cs
+++ src/PIM/Program.cs
@@ -4,7 +4,11 @@ using Starcounter;
 namespace PIM {
     class Program {
         static void Main() {
+            Handle.GET("/PIM/menu", () => {
+                return new Page() { Html = "/PIM/AppMenuPartial.html" };
+            });
 
+            UriMapping.Map("/PIM/menu", UriMapping.MappingUriPrefix + "/menu");
         }
     }
 }
