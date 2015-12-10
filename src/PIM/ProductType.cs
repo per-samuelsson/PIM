b0d4726e4704199e89c2b51ff9733b657945b70a
Per Samuelsson <per@starcounter.com>
2015-12-10 10:11:53 +0100

    Introducing the ProductType database model / class.

---
+++ src/PIM/ProductType.cs
@@ -0,0 +1,44 @@
+﻿using Starcounter;
+
+namespace PIM {
+
+    /// <summary>
+    /// The central PIM class: the one representing product types.
+    /// </summary>
+    [Database]
+    public class ProductType {
+        /// <summary>
+        /// Unique, user given identity of the current type;
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// A name to use to refer to the type by.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Product type description
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Url to an image illustrating the current type.
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// The parent type
+        /// </summary>
+        public ProductType Parent;
+
+        /// <summary>
+        /// Product type children (only the direct ones).
+        /// </summary>
+        public QueryResultRows<ProductType> Children {
+            get {
+                return Db.SQL<ProductType>("SELECT p FROM ProductType p WHERE p.Parent = ?", this);
+            }
+        }
+    }
+}
